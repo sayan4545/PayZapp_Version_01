25a7817b2a44d7daf7afdff36af0b2a972fe1021
sayan4545 <sayanchatterjee3010@gmail.com>
2026-08-03 20:23:01 +0530

    Code for the merchant signUp api endpoint

---
+++ src/main/java/com/chatterjee/sayan/payzapp/merchant/controllers/AuthController.java
@@ -0,0 +1,30 @@
+package com.chatterjee.sayan.payzapp.merchant.controllers;
+
+import com.chatterjee.sayan.payzapp.merchant.dtos.request.MerchantSignUpRequest;
+import com.chatterjee.sayan.payzapp.merchant.dtos.response.MerchantResponse;
+import com.chatterjee.sayan.payzapp.merchant.services.AuthService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/v1/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+
+    private final AuthService authService;
+
+    @PostMapping("/signUp")
+    public ResponseEntity<MerchantResponse> signUp(@RequestBody @Valid MerchantSignUpRequest merchantSignUpRequest){
+        return new ResponseEntity<>(authService.signUp(merchantSignUpRequest), HttpStatus.CREATED);
+
+    }
+
+
+}
