f9a7efe9b34b710bca8319afff59c2edac9e4291
sayan4545 <sayanchatterjee3010@gmail.com>
2026-08-04 19:58:39 +0530

    Wrote a repositorylayer

---
+++ src/main/java/com/chatterjee/sayan/payzapp/merchant/repositories/ApiKeyRepository.java
@@ -0,0 +1,11 @@
+package com.chatterjee.sayan.payzapp.merchant.repositories;
+
+import com.chatterjee.sayan.payzapp.merchant.entities.ApiKey;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+@Repository
+public interface ApiKeyRepository extends JpaRepository<ApiKey, UUID> {
+}
