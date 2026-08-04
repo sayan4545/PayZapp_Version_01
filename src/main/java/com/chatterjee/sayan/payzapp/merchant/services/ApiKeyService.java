d88bacaa65c2d51106b93be85c8da65fd3240d23
sayan4545 <sayanchatterjee3010@gmail.com>
2026-08-05 04:14:18 +0530

    Added method for rotating api key

--- src/main/java/com/chatterjee/sayan/payzapp/merchant/services/ApiKeyService.java
+++ src/main/java/com/chatterjee/sayan/payzapp/merchant/services/ApiKeyService.java
@@ -1,13 +1,24 @@
 package com.chatterjee.sayan.payzapp.merchant.services;
 
 import com.chatterjee.sayan.payzapp.merchant.dtos.request.CreateApiKeyRequest;
+import com.chatterjee.sayan.payzapp.merchant.dtos.response.ApiKeyGetResponse;
 import com.chatterjee.sayan.payzapp.merchant.dtos.response.ApiKeyResponse;
+import jakarta.annotation.Nullable;
 import jakarta.validation.Valid;
 import org.springframework.web.bind.annotation.RequestBody;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface ApiKeyService {
 
     ApiKeyResponse createApiKey(UUID merchantId, CreateApiKeyRequest createApiKeyRequest);
+
+    List<ApiKeyGetResponse> getAllApiKeys(UUID merchantId);
+
+    void revoke(UUID merchantId, UUID keyId);
+
+
+    @Nullable
+    ApiKeyResponse rotateApiKey(UUID merchantId, UUID keyId);
 }
