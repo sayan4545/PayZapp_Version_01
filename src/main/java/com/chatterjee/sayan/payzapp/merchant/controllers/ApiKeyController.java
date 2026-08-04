8eda1ae9bdcfba75dd9ddafdba30f99906db52bd
sayan4545 <sayanchatterjee3010@gmail.com>
2026-08-05 04:15:58 +0530

    Added REST end point for rotating apiKey and revoking apiKey for a merchant

--- src/main/java/com/chatterjee/sayan/payzapp/merchant/controllers/ApiKeyController.java
+++ src/main/java/com/chatterjee/sayan/payzapp/merchant/controllers/ApiKeyController.java
@@ -1,6 +1,7 @@
 package com.chatterjee.sayan.payzapp.merchant.controllers;
 
 import com.chatterjee.sayan.payzapp.merchant.dtos.request.CreateApiKeyRequest;
+import com.chatterjee.sayan.payzapp.merchant.dtos.response.ApiKeyGetResponse;
 import com.chatterjee.sayan.payzapp.merchant.dtos.response.ApiKeyResponse;
 import com.chatterjee.sayan.payzapp.merchant.entities.ApiKey;
 import com.chatterjee.sayan.payzapp.merchant.services.ApiKeyService;
@@ -11,6 +12,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 import java.util.UUID;
 
 @RestController
@@ -25,6 +27,24 @@ public class ApiKeyController {
     public ResponseEntity<ApiKeyResponse> createApiKey(@PathVariable UUID merchantId, @Valid @RequestBody CreateApiKeyRequest createApiKeyRequest) {
         return ResponseEntity.status(HttpStatus.CREATED)
                 .body(apiKeyService.createApiKey(merchantId,createApiKeyRequest));
+    }
+
+
+    @GetMapping
+    public ResponseEntity<List<ApiKeyGetResponse>> listByMerchant(@PathVariable UUID merchantId){
+        return ResponseEntity.status(HttpStatus.FOUND).body(apiKeyService.getAllApiKeys(merchantId));
+    }
+
+    @DeleteMapping("/{keyId}")
+    public ResponseEntity<Void> revoke(@PathVariable UUID merchantId, @PathVariable UUID keyId){
+
+        apiKeyService.revoke(merchantId,keyId);
+        return ResponseEntity.noContent().build();
+    }
+
+    @PostMapping("/{keyId}/rotate")
+    public ResponseEntity<ApiKeyResponse> rotate(@PathVariable UUID keyId,@PathVariable UUID merchantId){
+        return ResponseEntity.status(HttpStatus.OK).body(apiKeyService.rotateApiKey(merchantId,keyId));
     }
 
 
