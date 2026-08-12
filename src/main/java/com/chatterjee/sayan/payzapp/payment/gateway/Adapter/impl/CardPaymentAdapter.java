862fede42aef7a9fd87706ccd99184ec6e21081e
sayan4545 <sayanchatterjee3010@gmail.com>
2026-08-13 03:57:23 +0530

    Added CardPaymentAdapter implementation

--- src/main/java/com/chatterjee/sayan/payzapp/payment/gateway/Adapter/impl/CardPaymentAdapter.java
+++ src/main/java/com/chatterjee/sayan/payzapp/payment/gateway/Adapter/impl/CardPaymentAdapter.java
@@ -3,15 +3,33 @@ package com.chatterjee.sayan.payzapp.payment.gateway.Adapter.impl;
 import com.chatterjee.sayan.payzapp.payment.gateway.Adapter.PaymentAdapter;
 import com.chatterjee.sayan.payzapp.payment.gateway.dto.PaymentRequest;
 import com.chatterjee.sayan.payzapp.payment.gateway.dto.PaymentResult;
+import com.chatterjee.sayan.payzapp.payment.processor.dto.PaymentProcessorResponse;
+import com.chatterjee.sayan.payzapp.vault.service.VaultService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 import java.util.UUID;
 
 @Component
+@RequiredArgsConstructor
 public class CardPaymentAdapter implements PaymentAdapter {
+
+    private final VaultService vaultService;
     @Override
     public PaymentResult initiate(PaymentRequest paymentRequest) {
-        return null;
+        String token = (String) paymentRequest.methodDetails().get("token");
+
+        PaymentProcessorResponse response = vaultService.charge(paymentRequest.paymentId(),
+                token
+                ,paymentRequest.amount()
+                ,paymentRequest.methodDetails());
+
+        return switch (response){
+            case PaymentProcessorResponse.Success success-> new PaymentResult.Success(success.bankReference());
+            case PaymentProcessorResponse.Failed failed-> new PaymentResult.Failure(failed.errorCode(), failed.errorDescription());
+            case PaymentProcessorResponse.Pending pending -> new PaymentResult.Pending(pending.processorReference());
+        };
+
     }
 
     @Override
