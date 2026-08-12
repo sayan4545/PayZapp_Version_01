d3e29f64772c2de9f8bf0fa6ae76fab7b3402015
sayan4545 <sayanchatterjee3010@gmail.com>
2026-08-12 21:58:47 +0530

    Added VaultCardRepository and CardTokenRepository

---
+++ src/main/java/com/chatterjee/sayan/payzapp/vault/repository/CardTokenRepository.java
@@ -0,0 +1,10 @@
+package com.chatterjee.sayan.payzapp.vault.repository;
+
+import com.chatterjee.sayan.payzapp.vault.entities.CardToken;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+@Repository
+public interface CardTokenRepository extends JpaRepository<CardToken, UUID> {
+}
